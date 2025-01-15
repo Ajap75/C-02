c9a6be71a28c7badd72c2c84254565a9cc021551
Antoine Astruc <anastruc@paul-f4Br8s1.clusters.42paris.fr>
2025-01-15 12:52:42 +0100

    Correction mineur et implementation de test pour l'exercice 2

--- C++02/ex02/srcs/Fixed_arithmetic_operators.cpp
+++ C++02/ex02/srcs/Fixed_arithmetic_operators.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Fixed_arithmetic_operators.cpp                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: antoinejourdan-astruc <antoinejourdan-a    +#+  +:+       +#+        */
+/*   By: anastruc <anastruc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 14:48:10 by antoinejour       #+#    #+#             */
-/*   Updated: 2024/12/02 14:49:10 by antoinejour      ###   ########.fr       */
+/*   Updated: 2025/01/15 11:16:30 by anastruc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ Fixed Fixed::operator /(const Fixed& other) const
     if (other.FixedNumber == 0)
        throw std::invalid_argument("Division by zero");
     Fixed result;
-    result.FixedNumber = (FixedNumber << fractionnal_part / other.FixedNumber);
+    result.FixedNumber = (FixedNumber << fractionnal_part) / other.FixedNumber;
     return (result);
 }
 /*Division (/):
--- C++02/ex02/srcs/main.cpp
+++ C++02/ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: anastruc <anastruc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 16:31:06 by antoinejour       #+#    #+#             */
-/*   Updated: 2025/01/14 18:50:14 by anastruc         ###   ########.fr       */
+/*   Updated: 2025/01/15 11:35:05 by anastruc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,21 +25,36 @@ void    print_test(int i, const char *test, int c, int d)
         std::cout << YELLOW << "TEST : " << test << RED << "IS FALSE\n" << RESET << std::endl;
 }
 
-void    print_test_arithmetic(std::string test, int c, int d, const Fixed& result)
+void    print_test_arithmetic(std::string sign, int c, int d, const Fixed& result, int expected_result)
 {
+    
+    int i;
+    i = 0;
+    std::string test[4];
+    test[0] = "+";
+    test[1] = "-";
+    test[2] = "*";
+    test[3] = "/";
+
     std::cout << YELLOW << "c = " << c << " || d = " << d << RESET << std::endl;
-    if (!test.compare("+"))
+    // std::cout << sign.c_str() << std::endl << result.toInt() << std::endl << expected_result << std::endl;
+    while (i < 4)
     {
-        test = "c + d\n";
-        if (c + d == result.toInt())
-        {  
-            std::cout << YELLOW << "TEST : " << test << GREEN << "IS TRUE\n" << RESET << std::endl;
-        }
-        else 
-            std::cout << YELLOW << "TEST : " << test << RED << "IS FALSE\n" << RESET << std::endl;
-    }
-    else
+        if (!sign.compare(test[i]))
+        {
         
+            std::cout << YELLOW << "TEST : " << "c " << test[i] << " d" << RESET << std::endl;
+            std::cout << YELLOW << "RESULT = " << result.toInt() << " EXPECTED RESULT = " << expected_result << RESET << std::endl;
+            if (expected_result == result.toInt())
+            std::cout <<  GREEN << "THE TEST IS VALID\n" << RESET << std::endl;
+            else 
+                std::cout << RED << "THE TEST IS FALSE\n" << RESET << std::endl;
+            return;
+        }
+        i++;
+    }
+    if (i >= 4)
+            std::cout << RED << "Only +, -, * , / can be tested\n" << RESET << std::endl;
 }
 
 int main( void )
@@ -49,6 +64,7 @@ Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 Fixed c (5);
 Fixed d (2);
 int i;
+int expected_result;
 Fixed result;
 
 std::cout << a << std::endl;
@@ -59,8 +75,8 @@ std::cout << a << std::endl;
 std::cout << b << std::endl;
 std::cout << Fixed::max( a, b ) << std::endl;
 
-std::cout << "----------" << std::endl;
-std::cout << "------FFixed_comparison_operators-------" << std::endl;
+std::cout << MAGENTA << "---------------------------------------" << RESET << std::endl;
+std::cout << "------Fixed_comparison_operators-------" << std::endl;
 i = c > d;
 print_test(i, "c > d\n", c.toInt(), d.toInt());
 i = c < d;
@@ -74,17 +90,20 @@ print_test(i, "c == d\n", c.toInt(), d.toInt());
 i = c != d;
 print_test(i, "c != d\n", c.toInt(), d.toInt());
 
-std::cout << "----------" << std::endl;
+std::cout << MAGENTA << "---------------------------------------" << RESET << std::endl;
 std::cout << "------Fixed_arithmetic_operators-------" << std::endl;
 result = c + d;
-print_test_arithmetic("+", c.toInt(), d.toInt(), result);
+expected_result = c.toInt() + d.toInt();
+print_test_arithmetic("+", c.toInt(), d.toInt(), result, expected_result);
 result = c - d;
-print_test_arithmetic("-", c.toInt(), d.toInt(), result);
+expected_result = c.toInt() - d.toInt();
+print_test_arithmetic("-", c.toInt(), d.toInt(), result, expected_result);
 result = c * d;
-print_test_arithmetic("*", c.toInt(), d.toInt(), result);
+expected_result = c.toInt() * d.toInt();
+print_test_arithmetic("*", c.toInt(), d.toInt(), result, expected_result);
 result = c / d;
-print_test_arithmetic("/", c.toInt(), d.toInt(), result);
-
+expected_result = c.toInt() / d.toInt();
+print_test_arithmetic("/", c.toInt(), d.toInt(), result, expected_result);
 
 return 0;
 }
