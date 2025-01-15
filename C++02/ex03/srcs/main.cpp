eb53c4938ea2598aac2d023221509f9b29c45187
Antoine Astruc <anastruc@paul-f4Br8s1.clusters.42paris.fr>
2025-01-15 14:23:25 +0100

    derniere correction

--- C++02/ex03/srcs/main.cpp
+++ C++02/ex03/srcs/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: antoinejourdan-astruc <antoinejourdan-a    +#+  +:+       +#+        */
+/*   By: anastruc <anastruc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 16:31:06 by antoinejour       #+#    #+#             */
-/*   Updated: 2024/12/04 13:43:11 by antoinejour      ###   ########.fr       */
+/*   Updated: 2025/01/15 14:16:39 by anastruc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,22 +22,22 @@ void bspTest ()
     Point c(0, 10);
 
     Point Inside (3, 3);
-    std::cout << "Point Inside : " << (Point::bsp(a, b, c, Inside) ? "True" : "False") << std::endl;
+    std::cout << "Point Inside : " << std::endl << (Point::bsp(a, b, c, Inside) ? "True" : "False") << std::endl;
 
     Point onEdge(5, 0);
-    std::cout << "Point on edge: " << (Point::bsp(a, b, c, onEdge) ? "True" : "False") << std::endl;
+    std::cout << "Point on edge: " << std::endl << (Point::bsp(a, b, c, onEdge) ? "True" : "False") << std::endl;
 
     // At a vertex of the triangle
     Point onVertex(0, 0);
-    std::cout << "Point on vertex: " << (Point::bsp(a, b, c, onVertex) ? "True" : "False") << std::endl;
+    std::cout << "Point on vertex: " << std::endl << (Point::bsp(a, b, c, onVertex) ? "True" : "False") << std::endl;
 
     // Outside the triangle
     Point outside(15, 15);
-    std::cout << "Point outside: " << (Point::bsp(a, b, c, outside) ? "True" : "False") << std::endl;
+    std::cout << "Point outside: "<< std::endl << (Point::bsp(a, b, c, outside) ? "True" : "False") << std::endl;
 
     // Near the edge but outside (precision test)
     Point nearOutsideEdge(10.0001, -0.0001);
-    std::cout << "Point near edge (outside): " << (Point::bsp(a, b, c, nearOutsideEdge) ? "True" : "False") << std::endl;
+    std::cout << "Point near edge (outside): " << std::endl << (Point::bsp(a, b, c, nearOutsideEdge) ? "True" : "False") << std::endl;
 }
 
 int main ()
